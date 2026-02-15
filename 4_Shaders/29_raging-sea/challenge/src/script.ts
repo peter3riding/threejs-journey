de68022fc42cbac0bd9e8fa6dc0fb96004c39557
peterridingdev <peterdriding@gmail.com>
2026-02-15 18:34:40 +0400

    chore: start lesson 29-raging-sea challenge (TypeScript)

---
+++ 4_Shaders/29_raging-sea/challenge/src/script.ts
@@ -0,0 +1,101 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI({ width: 340 });
+
+// Canvas
+const canvas = document.querySelector<HTMLCanvasElement>("canvas.webgl");
+if (!canvas) throw new Error("Canvas not found");
+
+// Scene
+const scene = new THREE.Scene();
+
+// Loaders
+const textureLoader = new THREE.TextureLoader();
+
+/**
+ * Sizes
+ */
+
+interface Sizes {
+  width: number;
+  height: number;
+}
+
+const sizes: Sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", (): void => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  25,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.set(1.5, 0, 6);
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Test
+ */
+const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
+
+const waterMaterial = new THREE.ShaderMaterial();
+
+const waterObject = new THREE.Mesh(waterGeometry, waterMaterial);
+
+scene.add(waterObject);
+
+/**
+ * Animate
+ */
+const tick = (): void => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
