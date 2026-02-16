7876b42956f5cf0893a8e6411081c4824274b64a
peterridingdev <peterdriding@gmail.com>
2026-02-16 15:53:12 +0400

    feat: complete lesson 29-raging-sea challenge

--- 4_Shaders/29_raging-sea/challenge/src/script.ts
+++ 4_Shaders/29_raging-sea/challenge/src/script.ts
@@ -1,12 +1,24 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import waterVertexShader from "./shaders/water/vertex.glsl";
+import waterFragmentShader from "./shaders/water/fragment.glsl";
 
 /**
  * Base
  */
 // Debug
 const gui = new GUI({ width: 340 });
+const debugObject = {
+  depthColor: "#186691",
+  surfaceColor: "#9bd8ff",
+};
+gui.addColor(debugObject, "depthColor").onChange(() => {
+  waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
+});
+gui.addColor(debugObject, "surfaceColor").onChange(() => {
+  waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
+});
 
 // Canvas
 const canvas = document.querySelector<HTMLCanvasElement>("canvas.webgl");
@@ -17,6 +29,93 @@ const scene = new THREE.Scene();
 
 // Loaders
 const textureLoader = new THREE.TextureLoader();
+
+/**
+ * Raging Sea
+ */
+
+const waterMaterial = new THREE.ShaderMaterial({
+  vertexShader: waterVertexShader,
+  fragmentShader: waterFragmentShader,
+  uniforms: {
+    uTime: new THREE.Uniform(0),
+
+    uBigWavesFrequency: new THREE.Uniform(new THREE.Vector2(4, 1.5)),
+    uBigWavesElevation: new THREE.Uniform(0.2),
+    uBigWavesSpeed: new THREE.Uniform(0.75),
+
+    uSmallWavesElevation: { value: 0.15 },
+    uSmallWavesFrequency: { value: 3 },
+    uSmallWavesSpeed: { value: 0.2 },
+    uSmallIterations: { value: 4 },
+
+    uDepthColor: new THREE.Uniform(new THREE.Color(debugObject.depthColor)),
+    uSurfaceColor: new THREE.Uniform(new THREE.Color(debugObject.surfaceColor)),
+    uColorOffset: new THREE.Uniform(0.08),
+    uColorMultiplier: new THREE.Uniform(5),
+  },
+});
+
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "x")
+  .min(0)
+  .max(10)
+  .step(0.1)
+  .name("xFrequency");
+gui
+  .add(waterMaterial.uniforms.uBigWavesFrequency.value, "y")
+  .min(0)
+  .max(10)
+  .step(0.1)
+  .name("yFrequency");
+gui
+  .add(waterMaterial.uniforms.uBigWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("Wave Height");
+gui
+  .add(waterMaterial.uniforms.uBigWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("Wave Speed");
+gui
+  .add(waterMaterial.uniforms.uColorOffset, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uColorOffset");
+gui
+  .add(waterMaterial.uniforms.uColorMultiplier, "value")
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name("uColorMultiplier");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesElevation, "value")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .name("uSmallWavesElevation");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesFrequency, "value")
+  .min(0)
+  .max(30)
+  .step(0.001)
+  .name("uSmallWavesFrequency");
+gui
+  .add(waterMaterial.uniforms.uSmallWavesSpeed, "value")
+  .min(0)
+  .max(4)
+  .step(0.001)
+  .name("uSmallWavesSpeed");
+gui
+  .add(waterMaterial.uniforms.uSmallIterations, "value")
+  .min(0)
+  .max(5)
+  .step(1)
+  .name("uSmallIterations");
 
 /**
  * Sizes
@@ -78,11 +177,11 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
  */
 const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512);
 
-const waterMaterial = new THREE.ShaderMaterial();
-
 const waterObject = new THREE.Mesh(waterGeometry, waterMaterial);
-
+waterObject.rotation.x = -Math.PI * 0.5;
 scene.add(waterObject);
+
+const clock = new THREE.Clock();
 
 /**
  * Animate
@@ -90,6 +189,11 @@ scene.add(waterObject);
 const tick = (): void => {
   // Update controls
   controls.update();
+
+  // Water
+  const elapsedTime = clock.getElapsedTime();
+
+  waterMaterial.uniforms.uTime.value = elapsedTime;
 
   // Render
   renderer.render(scene, camera);
---
+++ 4_Shaders/29_raging-sea/challenge/src/shaders/water/vertex.glsl
@@ -0,0 +1,59 @@
+uniform float uTime;
+
+uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesElevation;
+uniform float uBigWavesSpeed;
+
+uniform float uSmallWavesElevation;
+uniform float uSmallWavesFrequency;
+uniform float uSmallWavesSpeed;
+uniform float uSmallIterations;
+
+varying float vElevation;
+
+#include ../includes/cnoise.glsl
+
+void main(){
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+ 
+  
+  float elevation = sin(modelPosition.x * uBigWavesFrequency.x + (uTime * uBigWavesSpeed)) * sin(modelPosition.z * uBigWavesFrequency.y + (uTime * uBigWavesSpeed)) * uBigWavesElevation;
+  
+
+  for(float i = 1.0; i <= uSmallIterations; i++){
+    elevation -= abs(cnoise(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i);
+  }
+
+  modelPosition.y += elevation;
+  
+  vec4 viewPosition =  viewMatrix * modelPosition;
+  gl_Position = projectionMatrix * viewPosition;
+
+  vElevation = modelPosition.y;
+}
+
+// uniform vec2 uFrequency;
+// uniform float uHeight;
+// uniform float uTime;
+// uniform float uSpeed;
+
+
+// void main(){
+
+
+  
+ 
+ 
+
+//   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+//   // y axis
+//   float elevation = sin(modelPosition.x * uFrequency.x + uTime * uSpeed) * sin(modelPosition.z * uFrequency.y + uTime * uSpeed) * uHeight;
+  
+
+ 
+
+//   modelPosition.y += elevation;
+
+//   vec4 viewPosition =  viewMatrix * modelPosition;
+//   gl_Position = projectionMatrix * viewPosition;
+// }
