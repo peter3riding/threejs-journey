1f86635e7a5e63ccfdbdd7e6876480d09f111df3
peterridingdev <peterdriding@gmail.com>
2026-03-03 15:48:11 +0400

    feat: complete 35-lights-shading

--- 4_Shaders/35_Lights-Shading-Shaders/starter/src/shaders/shading/vertex.glsl
+++ 4_Shaders/35_Lights-Shading-Shaders/starter/src/shaders/shading/vertex.glsl
@@ -1,6 +1,15 @@
+varying vec3 vNormal;
+varying vec3 vPosition;
+
 void main()
-{
+{   
+    
+
     // Position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
+
+    // Varying
+    vNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+    vPosition = modelPosition.xyz;
 }
