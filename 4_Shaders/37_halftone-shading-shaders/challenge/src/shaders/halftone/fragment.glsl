487b6a0c761cd2a8064a038d250ca0480d8706e4
peterridingdev <peterdriding@gmail.com>
2026-03-15 12:22:07 +0400

    feat: complete challenge 37-halftone-shading

---
+++ 4_Shaders/37_halftone-shading-shaders/challenge/src/shaders/halftone/fragment.glsl
@@ -0,0 +1,54 @@
+uniform vec3 uColor;
+uniform vec2 uResolution;
+
+varying vec3 vNormal;
+varying vec3 vPosition;
+
+#include ../includes/ambientLight.glsl
+#include ../includes/directionalLight.glsl
+
+
+
+void main()
+{
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    vec3 normal = normalize(vNormal);
+    vec3 color = uColor;
+
+     // Lights
+    vec3 light = vec3(0.0);
+
+      light += ambientLight(
+        vec3(1.0), // Light color
+        1.0        // Light intensity,
+    );
+
+     light += directionalLight(
+        vec3(1.0, 1.0, 1.0), // Light color
+        1.0,                 // Light intensity
+        normal,              // Normal
+        vec3(1.0, 1.0, 0.0), // Light position
+        viewDirection,       // View direction
+        1.0                  // Specular power
+    );
+    color *= light;
+    
+    vec3 patternDirection = vec3(0.0, -1.0, 0.0);
+    vec3 pointColor = vec3(1.0, 0.0, 0.0);
+    float intensity = dot(patternDirection, normal);
+    intensity = smoothstep(-0.5, 1.5, intensity);
+    vec2 uv = gl_FragCoord.xy / uResolution.y;
+    uv = mod(uv* 50.0, 1.0);
+
+    float point = distance(uv, vec2(0.5));
+    point = 1.0 - step(0.5 * intensity, point);
+
+
+    color = mix(color, pointColor, point);
+
+    // Final color
+     gl_FragColor = vec4(color, 1.0);
+     
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
