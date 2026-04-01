176c1302b1689e6a3fa6eb6a8df28407a320b54b
peterridingdev <peterdriding@gmail.com>
2026-04-01 15:30:14 +0400

    feat: complete 40-particles-morphing

--- 4_Shaders/40_particles-morphing/challenge/src/shaders/particles/fragment.glsl
+++ 4_Shaders/40_particles-morphing/challenge/src/shaders/particles/fragment.glsl
@@ -1,6 +1,12 @@
+varying vec3 vColor;
+
 void main()
-{
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+{   
+    float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
+    float alpha = 0.05/ distanceToCenter - 0.1;
+    
+
+    gl_FragColor = vec4(vec3(vColor), alpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
