605a400fb5d18135ffec0573df3fff57a980bffc
peterridingdev <peterdriding@gmail.com>
2026-04-20 16:49:47 +0400

    feat: finish 45-post-processing

--- 5_Extra/45_post-processing/tsl-port/src/main.ts
+++ 5_Extra/45_post-processing/tsl-port/src/main.ts
@@ -8,6 +8,10 @@ import {
   vec3,
   vec4,
   float,
+  screenUV,
+  Fn,
+  dot,
+  normalize,
 
   // Time & Animation
   time,
@@ -48,6 +52,9 @@ import { RenderPipeline } from "three/webgpu";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { dotScreen } from "three/addons/tsl/display/DotScreenNode.js";
+import { rgbShift } from "three/addons/tsl/display/RGBShiftNode.js";
+import { smaa } from "three/addons/tsl/display/SMAANode.js";
+import BloomNode, { bloom } from "three/addons/tsl/display/BloomNode.js";
 
 (async () => {
   /**
@@ -68,6 +75,10 @@ import { dotScreen } from "three/addons/tsl/display/DotScreenNode.js";
   const gltfLoader = new GLTFLoader();
   const cubeTextureLoader = new THREE.CubeTextureLoader();
   const textureLoader = new THREE.TextureLoader();
+
+  const uNormalMap = texture(
+    textureLoader.load("/textures/interfaceNormalMap.png"),
+  );
 
   /**
    * Update all materials (for MeshStandardMaterial models)
@@ -183,17 +194,86 @@ import { dotScreen } from "three/addons/tsl/display/DotScreenNode.js";
   const postProcessing = new THREE.RenderPipeline(renderer);
   postProcessing.outputColorTransform = false;
 
+  // First pass
   const scenePass = pass(scene, camera);
 
+  // DOT NODE
+  const uAngle = uniform(1.57); // radians
+  const uScale = uniform(1.0);
+
   const dottedNode = dotScreen(
-    scenePass.getTextureNode("output"), // input from the scene
-    uniform(1.57), // angle (radians) — you can make this a uniform too
-    uniform(1.0), // scale
+    scenePass.getTextureNode("output"),
+    1.57, // initial angle (number)
+    1.0, // initial scale (number)
+  );
+  dottedNode.angle = uAngle;
+  dottedNode.scale = uScale;
+
+  //const outputPass = renderOutput(dottedNode);
+
+  // RGBSHIFT
+  const uRGBAmount = uniform(0.005); // strength of the chromatic aberration
+  const uRGBAngle = uniform(0); // angle in radians
+
+  const rgbShiftNode = rgbShift(
+    scenePass.getTextureNode("output"),
+    0.005, // initial amount (number)
+    0, // initial angle (number)
   );
 
-  const outputPass = renderOutput(scenePass);
+  rgbShiftNode.amount = uRGBAmount;
+  rgbShiftNode.angle = uRGBAngle;
 
-  postProcessing.outputNode = outputPass;
+  // const outputPass = renderOutput(smaa(rgbShiftNode));
+
+  //__ BLOOM
+  const uBloomStrength = uniform(1.0);
+  const uBloomRadius = uniform(0.5);
+  const uBloomThreshold = uniform(0.5);
+
+  const bloomNode = bloom(
+    rgbShiftNode, // input from previous effect
+    1.0, // initial strength
+    0.5, // initial radius
+    0.5, // initial threshold
+  );
+  bloomNode.strength = uBloomStrength;
+  bloomNode.radius = uBloomRadius;
+  bloomNode.threshold = uBloomThreshold;
+
+  // postProcessing.outputNode = renderOutput(smaa(bloomNode));
+
+  // TINT PASS
+  const tintValue = new THREE.Vector3(0, 0, 0);
+  const uTint = uniform(tintValue);
+
+  const tintNode = Fn(([input]: [any]) => {
+    const color = input.sample(screenUV);
+    return color.add(uTint); // uTint is a vec3 you control
+  });
+
+  // postProcessing.outputNode = renderOutput(
+  //   smaa(tintNode(scenePass.getTextureNode())),
+  // );
+
+  // DISPLACEMENT NODE
+  const displacementNode = Fn(([input]: [any]) => {
+    const normalColor = uNormalMap.sample(screenUV).rgb.mul(2).sub(1);
+    const newUv = screenUV.add(normalColor.xy.mul(0.1));
+
+    let color = input.sample(newUv);
+
+    // Simple fake lighting like in the lesson
+    const lightDir = normalize(vec3(-1, 1, 0));
+    const lightness = clamp(dot(normalColor, lightDir), 0, 1);
+    color = color.add(lightness.mul(2));
+
+    return color;
+  });
+
+  postProcessing.outputNode = renderOutput(
+    smaa(displacementNode(scenePass.getTextureNode())),
+  );
 
   /**
    * Resize handler
@@ -208,6 +288,16 @@ import { dotScreen } from "three/addons/tsl/display/DotScreenNode.js";
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   });
+
+  // Tweaks
+  gui.add(uAngle, "value", 0, Math.PI * 2, 0.01).name("Dot Angle");
+  gui.add(uScale, "value", 0.2, 5, 0.01).name("Dot Scale");
+  gui.add(uBloomStrength, "value", 0, 3, 0.01).name("Bloom Strength");
+  gui.add(uBloomRadius, "value", 0, 2, 0.01).name("Bloom Radius");
+  gui.add(uBloomThreshold, "value", 0, 1, 0.01).name("Bloom Threshold");
+  gui.add(tintValue, "x", -1, 1).name("red");
+  gui.add(tintValue, "y", -1, 1).name("green");
+  gui.add(tintValue, "z", -1, 1).name("blue");
 
   /**
    * Animate
