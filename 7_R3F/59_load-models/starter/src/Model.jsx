73a12d5c66678df61391221f5631e4d25f9bb305
peterridingdev <peterdriding@gmail.com>
2026-05-05 12:00:09 +0400

    feat: finish 59-load-models

---
+++ 7_R3F/59_load-models/starter/src/Model.jsx
@@ -0,0 +1,29 @@
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { Clone, useGLTF } from "@react-three/drei";
+
+export default function Model() {
+  // const model = useGLTF("./hamburger.glb");
+  const model = useGLTF("./hamburger-draco.glb");
+
+  useGLTF.preload("./hamburger-draco.glb");
+
+  // const model = useLoader(
+  //   GLTFLoader,
+  //   "./FlightHelmet/glTF/FlightHelmet.gltf",
+  //   (loader) => {
+  //     const dracoLoader = new DRACOLoader();
+  //     dracoLoader.setDecoderPath("./draco/");
+  //     loader.setDRACOLoader(dracoLoader);
+  //   },
+  // );
+
+  return (
+    <>
+      <Clone object={model.scene} scale={0.35} position-x={-4} />
+      <Clone object={model.scene} scale={0.35} position-x={0} />
+      <Clone object={model.scene} scale={0.35} position-x={4} />
+    </>
+  );
+}
