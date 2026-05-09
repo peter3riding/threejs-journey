d6f037150b30cb4eeb625f77ed467e74de3030a9
peterridingdev <peterdriding@gmail.com>
2026-05-09 13:13:56 +0400

    chore: begin 63-post-processing

---
+++ 7_R3F/63_post-processing/final/src/Drunk.jsx
@@ -0,0 +1,8 @@
+import DrunkEffect from './DrunkEffect.jsx'
+
+export default function Drunk(props)
+{
+    const effect = new DrunkEffect(props)
+    
+    return <primitive ref={ props.ref } object={ effect } />
+}
